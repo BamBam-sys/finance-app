73afd213144c175571c259ee46b97a242094e1d8
USER <user@TIFE.local>
2025-04-15 22:11:27 +0100

    temporaryily fix lint error

--- src/app/transactions/components/TransactionList.tsx
+++ src/app/transactions/components/TransactionList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 import React, { HTMLAttributes, useEffect, useState } from 'react';
 import TransactionItem from './TransactionItem';
